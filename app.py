614ea7fd216e19bee9b0f44276e08c3df70767db
IamRaNo <70652176+IamRaNo@users.noreply.github.com>
2025-06-25 13:46:18 +0530

    changed the front end

--- app.py
+++ app.py
@@ -1,15 +1,23 @@
 import streamlit as st
 import pickle
 
-st.title('Movie Recommended System')
+# Load the model and data
 with open('movies.pkl', 'rb') as file:
     data = pickle.load(file)
+
 with open('similarity.pkl', 'rb') as file:
     similarity = pickle.load(file)
+
+# App title and description
+st.markdown("<h1 style='text-align: center; color: #ff4b4b;'>🎬 Movie Recommendation System</h1>", unsafe_allow_html=True)
+st.markdown("<h4 style='text-align: center;'>Find movies similar to your favorite one!</h4>", unsafe_allow_html=True)
+st.write("")
+
+# Dropdown selection
 movies_list = data['title'].values
-selected_movie = st.selectbox('Enter movie name', movies_list)
+selected_movie = st.selectbox('Search or choose a movie:', movies_list)
 
-
+# Recommendation function
 def recommend(movie):
     recommendation_list = []
     movie_index = data[data['title'] == movie].index[0]
@@ -19,8 +27,9 @@ def recommend(movie):
         recommendation_list.append(data.iloc[movie[0]].title)
     return recommendation_list
 
-
-if st.button('Recommend'):
+# Recommend button
+if st.button('🎥 Recommend Movies'):
     recommendation = recommend(selected_movie)
-    for i in recommendation:
-        st.write(i)
+    st.markdown("## ⭐ Top 5 Recommendations for You:")
+    for idx, title in enumerate(recommendation, 1):
+        st.markdown(f"**{idx}.** 🎞️ {title}")
